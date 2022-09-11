2c5f058fd01df8fea18791ea8d3fd207600619ef
Gabriel Rizzi <75540693+Rizzi-dev@users.noreply.github.com>
2022-09-11 15:47:04 -0300

    Update DaoTest.java

--- sistema-de-veiculos/src/test/java/util/DaoTest.java
+++ sistema-de-veiculos/src/test/java/util/DaoTest.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package util;
 
 import java.util.List;
@@ -17,7 +13,7 @@ import model.Veiculo;
 import util.Dao;
 /**
  *
- * @author prog2
+ * @author Rizzi
  */
 public class DaoTest {
     
@@ -40,54 +36,5 @@ public class DaoTest {
     public void tearDown() {
     }
 
-//    @Test
-//    public void testInserir() {
-//        Veiculo v = new Veiculo("Ford", "Mustang", "12345");
-//        Dao<Veiculo> dao = new Dao(Veiculo.class);
-//        dao.inserir(v);
-//    }
-    
-//    @Test
-//    public void testListar() {
-//        Dao<Veiculo> dao = new Dao(Veiculo.class);
-//        List<Veiculo> lista = dao.listarTodos();
-//        for(Veiculo v : lista){
-//            System.out.println(v);
-//        }
-//    }
-   
-//    @Test
-//    public void testInserirMotorista() {
-//        Endereco endereco = new Endereco();
-//        endereco.setCep("19930-000");
-//        endereco.setLocalidade("Ribeirao do sul");
-//        endereco.setLogradouro("Rua tiradentes");
-//        endereco.setComplemento("Portao preto");
-//        endereco.setBairro("Cabeceira");
-//        endereco.setUf("SP");
-//        
-//        Motorista m = new Motorista("123321", "Astolfo");
-//        m.setNome("Gabriel Lima");
-//        m.setEndereco(endereco);
-//        Dao<Motorista> dao = new Dao(Motorista.class);
-//        dao.inserir(m);
-//    }
 
-//    @Test
-//    public void testInserirOperador() {
-//        Endereco endereco = new Endereco();
-//        endereco.setCep("19930-000");
-//        endereco.setLocalidade("Ribeirao do sul");
-//        endereco.setLogradouro("Rua tiradentes");
-//        endereco.setComplemento("Portao preto");
-//        endereco.setBairro("Cabeceira");
-//        endereco.setUf("SP");
-//
-//        Operador o = new Operador("prog2", "banana123*");
-//        o.setNome("Gabriel Lima");
-//        o.setEndereco(endereco);
-//        Dao<Operador> dao = new Dao(Operador.class);
-//        dao.inserir(o);
-//    }
-    
 }
